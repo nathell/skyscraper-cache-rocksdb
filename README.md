fda0a969092d8c5008e88c6cf7947c9310891328
Daniel Janus <dj@danieljanus.pl>
2022-01-01 23:25:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# skyscraper-cache-rocksdb
+
+A cache backend for [Skyscraper][1] (version 0.3+) based on [RocksDB][2].
+
+Status: experimental.
+
+## License
+
+MIT (see `LICENSE`).
+
+ [1]: https://github.com/nathell/skyscraper
+ [2]: https://rocksdb.org/
---
+++ src/skyscraper/cache/rocksdb.clj
@@ -0,0 +1,32 @@
+(ns skyscraper.cache.rocksdb
+  (:require [skyscraper.cache :as cache]
+            [taoensso.nippy :as nippy])
+  (:import [skyscraper.cache CacheBackend]
+           [org.rocksdb CompressionType Options RocksDB]))
+
+(defn- db-keys [key]
+  {:meta-key (str "meta/" key)
+   :blob-key (str "blobs/" key)})
+
+(deftype RocksDBCache
+    [db]
+  CacheBackend
+  (save-blob [cache key blob metadata]
+    (let [{:keys [meta-key blob-key]} (db-keys key)]
+      (.put db (nippy/freeze meta-key) (nippy/freeze metadata))
+      (.put db (nippy/freeze blob-key) blob)))
+  (load-blob [cache key]
+    (let [{:keys [meta-key blob-key]} (db-keys key)
+          meta (.get db (nippy/freeze meta-key))
+          blob (.get db (nippy/freeze blob-key))]
+      (when (and meta blob)
+        {:meta (nippy/thaw meta)
+         :blob blob}))))
+
+(defn rocks-db-cache [dir]
+  (let [opts (doto (Options.)
+               (.setCreateIfMissing true)
+               (.setCompressionType CompressionType/LZ4_COMPRESSION)
+               (.setBottommostCompressionType CompressionType/ZSTD_COMPRESSION))
+        db (RocksDB/open opts dir)]
+    (RocksDBCache. db)))
